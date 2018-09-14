5baa5fbd0c9cfeaf73e589a07f163d82dbd46a0b
Bjørvik, Erik <Erik.Bjorvik@skandiabanken.no>
2018-09-14 15:47:00 +0200

    initial commit

---
+++ Nu3Admin/Controllers/BeaconsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nu3Admin.Controllers
+{
+    public class BeaconsController : Controller
+    {
+        // GET: Beacons
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nu3Admin/Models/Consumable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nu3Admin.Models
+{
+    public class Consumable
+    {
+    }
+}
